bc771619b9e2a756a609bd8c656ec717456c0fde
driminicus <mmatthhii@hotmail.com>
2017-04-21 17:04:21 +0200

    put volume path and real path in variables

--- wanted/search-wanted.sh
+++ wanted/search-wanted.sh
@@ -4,14 +4,15 @@ declare WANTED_FILE=`dirname $0`/subs.wanted
 
 declare MISSED=""
 echo "###### Process started at: $(date) ######"
-
+INVOLUMEPATH='\/tv\/'
+REALPATH='\/data\/nethdd\/Series\/'
 while IFS=':' read -a line; do
   SOURCE=${line[0]}
   SRT=${line[1]}
   LANG=`echo $SRT | sed -e "s/\.srt//g" -e "s/.*\(..\)/\1/"`
   echo "subliminal download -l $LANG $SOURCE"
   docker run --rm --name subliminal -v subliminal_cache:/usr/src/cache -v /data/nethdd/Series:/tv -i diaoulael/subliminal download -l ${LANGUAGES}  "${EPISODE_PATH}" >> $LOG_FILE 2>&1
-  SEARCHSRT=`echo $SRT | sed -e 's/tv/data\/nethdd\/Series/g'`
+  SEARCHSRT=`echo $SRT | sed -e "s/$INVOLUMEPATH/$REALPATH/g"`
   if [[ ! -f $SEARCHSRT ]]; then
     IFS=''
     MISSED="$SOURCE:$SRT\n$MISSED"

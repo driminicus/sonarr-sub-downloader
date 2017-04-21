0c2c1f7874f66799512fb937545e9b32a729eedd
driminicus <mmatthhii@hotmail.com>
2017-04-21 16:49:38 +0200

    Search for subtitlefile not in container but real folder

--- wanted/search-wanted.sh
+++ wanted/search-wanted.sh
@@ -11,8 +11,8 @@ while IFS=':' read -a line; do
   LANG=`echo $SRT | sed -e "s/\.srt//g" -e "s/.*\(..\)/\1/"`
   echo "subliminal download -l $LANG $SOURCE"
   docker run --rm --name subliminal -v subliminal_cache:/usr/src/cache -v /data/nethdd/Series:/tv -i diaoulael/subliminal download -l ${LANGUAGES}  "${EPISODE_PATH}" >> $LOG_FILE 2>&1
-
-  if [[ ! -f $SRT ]]; then
+  SEARCHSRT=`echo $SRT | sed -e 's/tv/data\/nethdd\/Series/g'`
+  if [[ ! -f $SEARCHSRT ]]; then
     IFS=''
     MISSED="$SOURCE:$SRT\n$MISSED"
     echo "Subtitle still not available"
